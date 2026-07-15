4c7856c83f5425a4d7da7bddc421f8e44521c998
Kaivalya-04 <kaivalyachandrayan6969@gmail.com>
2026-07-15 11:32:37 +0530

    Update driver.js

--- driver.js
+++ driver.js
@@ -1,1 +1,30 @@
-
+const drivers = [
+  {
+    position: 1,
+    code: "LEC",
+    team: "Ferrari",
+    tyre: "S",
+    gap: "Leader"
+  },
+  {
+    position: 2,
+    code: "HAM",
+    team: "Ferrari",
+    tyre: "M",
+    gap: "+1.842"
+  },
+  {
+    position: 3,
+    code: "VER",
+    team: "Red Bull",
+    tyre: "M",
+    gap: "+4.091"
+  },
+  {
+    position: 4,
+    code: "NOR",
+    team: "McLaren",
+    tyre: "H",
+    gap: "+5.733"
+  }
+];
