2553c3a8e5547b3c06896b1c83b2d087687be6da
Kaivalya-04 <kaivalyachandrayan6969@gmail.com>
2026-07-16 16:40:32 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
+const clock = document.getElementById("clock");
 
+function updateClock() {
+    if (!clock) return;
+
+    const now = new Date();
+
+    const h = String(now.getHours()).padStart(2, "0");
+    const m = String(now.getMinutes()).padStart(2, "0");
+    const s = String(now.getSeconds()).padStart(2, "0");
+
+    clock.textContent = `${h}:${m}:${s}`;
+}
+
+updateClock();
+setInterval(updateClock, 1000);
