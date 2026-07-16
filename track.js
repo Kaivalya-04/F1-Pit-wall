0c1eedc520b986852c0fd90910bab4ed026cbf58
Kaivalya-04 <kaivalyachandrayan6969@gmail.com>
2026-07-16 16:07:44 +0530

    Update track.js

--- track.js
+++ track.js
@@ -1,7 +1,35 @@
 const track = document.getElementById("track");
 
-if (track) {
-    track.innerHTML = "<h2 style='color:lime'>TRACK.JS IS WORKING</h2>";
-} else {
-    alert("Track div not found!");
-}
+track.innerHTML = `
+<svg id="trackSvg" viewBox="0 0 800 500" width="100%" height="400">
+    <ellipse
+        cx="400"
+        cy="250"
+        rx="300"
+        ry="120"
+        fill="none"
+        stroke="#888"
+        stroke-width="8"
+        id="trackPath"
+    />
+</svg>
+`;
+
+const svg = document.getElementById("trackSvg");
+const path = document.getElementById("trackPath");
+
+const length = path.getTotalLength();
+
+drivers.forEach((driver, i) => {
+    const car = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+
+    car.setAttribute("r", "6");
+    car.setAttribute("fill", i < 2 ? "red" : "#00ff99");
+
+    svg.appendChild(car);
+
+    const point = path.getPointAtLength((length / drivers.length) * i);
+
+    car.setAttribute("cx", point.x);
+    car.setAttribute("cy", point.y);
+});
