36dc8b928be91f746daaa00e00dc9b4ec442e4da
Kaivalya-04 <kaivalyachandrayan6969@gmail.com>
2026-07-16 16:38:48 +0530

    Update timing.js

--- js/timing.js
+++ js/timing.js
@@ -1,1 +1,21 @@
+const tower = document.getElementById("timingTowerContents");
 
+function buildTimingTower() {
+    if (!tower) return;
+
+    tower.innerHTML = "<h2>Timing Tower</h2>";
+
+    drivers.forEach(driver => {
+        tower.innerHTML += `
+        <div class="driverRow">
+            <div class="position">P${driver.position}</div>
+            <div class="driverCode">${driver.code}</div>
+            <div class="team">${driver.team}</div>
+            <div class="tyre">${driver.tyre}</div>
+            <div class="gap">${driver.gap}</div>
+        </div>
+        `;
+    });
+}
+
+buildTimingTower();
