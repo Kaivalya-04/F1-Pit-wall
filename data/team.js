d5b0a80d0a3542fb7390313e7b80bbca5f862737
Kaivalya-04 <kaivalyachandrayan6969@gmail.com>
2026-09-15 13:51:18 +0530

    Update team.js

--- data/team.js
+++ data/team.js
@@ -1,1 +1,75 @@
+/* =========================================
+   SCUDERIA COMMAND
+   TEAM DATABASE
+   2026
+========================================= */
 
+const teams = [
+
+    {
+        id: "MCL",
+        name: "McLaren",
+        drivers: ["NOR", "PIA"]
+    },
+
+    {
+        id: "MER",
+        name: "Mercedes",
+        drivers: ["ANT", "RUS"]
+    },
+
+    {
+        id: "FER",
+        name: "Ferrari",
+        drivers: ["HAM", "LEC"]
+    },
+
+    {
+        id: "RBR",
+        name: "Red Bull Racing",
+        drivers: ["VER", "HAD"]
+    },
+
+    {
+        id: "RB",
+        name: "Racing Bulls",
+        drivers: ["LAW", "LIN"]
+    },
+
+    {
+        id: "ALP",
+        name: "Alpine",
+        drivers: ["GAS", "COL"]
+    },
+
+    {
+        id: "HAS",
+        name: "Haas F1 Team",
+        drivers: ["OCO", "BEA"]
+    },
+
+    {
+        id: "AUD",
+        name: "Audi",
+        drivers: ["HUL", "BOR"]
+    },
+
+    {
+        id: "WIL",
+        name: "Williams",
+        drivers: ["SAI", "ALB"]
+    },
+
+    {
+        id: "AMR",
+        name: "Aston Martin",
+        drivers: ["ALO", "STR"]
+    },
+
+    {
+        id: "CAD",
+        name: "Cadillac",
+        drivers: ["PER", "BOT"]
+    }
+
+];
